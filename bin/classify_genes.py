9d11a8de563cdd5a6047d9e25fbbbddc14d53cbd
Latif Raditya Rusdi <115939908+latifraditya@users.noreply.github.com>
2024-10-29 12:18:35 +0700

    Update classify_genes.py

--- bin/classify_genes.py
+++ bin/classify_genes.py
@@ -68,10 +68,9 @@ def normalize(v):
 	return [v[i]/vmag for i in range(len(v))]
 
 def vect_mu(v_list, l):
-	v_mu=[0.0]*l
-	for v in v_list:
-		v_mu=map(sum, zip(v_mu, v))
-	return normalize(v_mu)
+	v=np.array(v_list)
+	vsum=np.ndarray.sum(v,axis=0)
+	return normalize(vsum)
 
 def getgenexpr(fname, p, tp, dt):
 	f=open(fname, "r")
@@ -88,8 +87,8 @@ def getgenexpr(fname, p, tp, dt):
 			tok=line.strip().split("\t")
 			for i in tok[1:p*tp]:
 				pid, tid=i.split("_")
-				p_list.append(pid)
-				tp_list.append(tid)
+				p_list.append(pid.strip())
+				tp_list.append(tid.strip())
 			continue
 		tok=line.strip().split("\t")
 		gid=tok[0]
@@ -136,7 +135,7 @@ def read_clusters(fname, g, pid, tpid, dep): # cluster file, gene expression fil
 		for idx, i in enumerate(pid):
 			if i not in kc[kclid].pheno:
 				kc[kclid].pheno[i]=Phenotype()
-			expr=map(float, g[gid])
+			expr=[float(i) for i in g[gid]]
 			v=expr[idx*tp:(idx*tp)+tp]
 
 			kc[kclid].pheno[i].vect.append(v)
@@ -275,7 +274,7 @@ def Rescue_test(kc, pid, p_n, t_n):
 			v=kc[k].k_gid[g]
 
 			min_dist=sys.float_info.max
-			min_k=-1
+			min_k=""
 			for k_j in sorted(kc,key=int):
 				if kc[k_j].type == "NEP": continue
 				dist=distance.cosine(kc[k_j].k_skmeans_cent, v)
@@ -284,7 +283,7 @@ def Rescue_test(kc, pid, p_n, t_n):
 					min_dist=dist
 					min_k=k_j
 
-			if min_k > -1:
+			if min_k != "":
 				rg.add(g)
 
 				kc[min_k].k_gid[g]=kc[k].k_gid[g]
@@ -374,7 +373,7 @@ def update(kc):
 			if len(p.gid) < 2:
 				kc[k].gene_count=len(p.gid)
 				break
-			p.cent=vect_mu(p.vect, kc[k].n_tp)	# updating p centroid
+			p.cent=vect_mu(p.vect, kc[k].n_tp)			# updating p centroid
 			pcents.append(p.cent)
 			kc[k].gene_count=len(p.gid)
 
@@ -408,54 +407,53 @@ def print_cluster(kc):
 		cls[kc[k].type]+=len(k_gids)
 
 	for c in cls:
-		print c, cls[c], len(gids)
+		print (c, cls[c], len(gids))
 
 def get_minmax(kc):
 	vs=[]
 	for k in kc:
-
 		for v in kc[k].k_skmeans_cent:
 			vs.append(v)
-
 	min_v=min(vs)
 	max_v=max(vs)
-	
 	return [min_v, max_v]
 
 def results_to_file(kc, path, pids, tps, cv, ge, dt, ge_f):
 
 	# parameter configuration log
-	param_f=open("%s/param.conf"%(path), "w")					# parameter configuration log
-	param_f.write("class=")										# class
+	param_f=open("%s/param.conf"%(path), "w")				# parameter configuration log
+	param_f.write("class=")							# class
 	for p in pids[0:-1]:
 		param_f.write("%s,"%(p))
 	param_f.write("%s\n"%(pids[-1]))
 	
-	param_f.write("timepoints=")								# timepoints
+	param_f.write("timepoints=")						# timepoints
 	for tp in tps[0:-1]:
 		param_f.write("%s,"%(tp))
 	param_f.write("%s\n"%(tps[-1]))
 
-	param_f.write("expressionfile=%s\n"%(ge_f))					# expression file
-	param_f.write("datatype=%s\n"%(dt))							# datatype
+	param_f.write("expressionfile=%s\n"%(ge_f))				# expression file
+	param_f.write("datatype=%s\n"%(dt))					# datatype
 	param_f.close()
 	
 	
 	# writing data to files
 	depc_f=open("%s/DEP_clusters.dat"%(path), "w")				# DEP cluster output file
-	depg_f=open("%s/DEP_genes.dat"%(path), "w")					# DEP gene output file
-	depc_plot_f=open("%s/plots/DEP_clusters_plot.dat"%(path), "w")	# DEP cluster output file for plotting
+	depg_f=open("%s/DEP_genes.dat"%(path), "w")				# DEP gene output file
+	depc_plot_f=open("%s/plots/DEP_clusters_plot.dat"%(path), "w")		# DEP cluster output file for plotting
 	depg_plot_f=open("%s/plots/DEP_genes_plot.dat"%(path), "w")		# DEP gene output file
-
+	depg_norm_plot_f=open("%s/plots/DEP_genes_norm_plot.dat"%(path), "w")	# DEP gene normalized output file
+	
 	sepc_f=open("%s/SEP_clusters.dat"%(path), "w")				# SEP cluster output file
-	sepg_f=open("%s/SEP_genes.dat"%(path), "w")					# SEP gene file
-	sepc_plot_f=open("%s/plots/SEP_clusters_plot.dat"%(path), "w")	# SEP cluster output file
+	sepg_f=open("%s/SEP_genes.dat"%(path), "w")				# SEP gene file
+	sepc_plot_f=open("%s/plots/SEP_clusters_plot.dat"%(path), "w")		# SEP cluster output file
 	sepg_plot_f=open("%s/plots/SEP_genes_plot.dat"%(path), "w")		# SEP gene file
-
+	sepg_norm_plot_f=open("%s/plots/SEP_genes_norm_plot.dat"%(path), "w")	# SEP gene normalized file
+	
 	cluster_files={"DEP":depc_f, "SEP":sepc_f}
 	gene_files={"DEP":depg_f, "SEP":sepg_f}
 	cluster_plot_files={"DEP":depc_plot_f, "SEP":sepc_plot_f}
-	gene_plot_files={"DEP":depg_plot_f, "SEP":sepg_plot_f}
+	gene_plot_files={"DEP":depg_plot_f, "SEP":sepg_plot_f, "DEP_norm":depg_norm_plot_f, "SEP_norm":sepg_norm_plot_f}
 
 	# writing headers
 	for f in cluster_plot_files:
@@ -465,7 +463,7 @@ def results_to_file(kc, path, pids, tps, cv, ge, dt, ge_f):
 
 	for f in cluster_files:
 		cluster_files[f].write("Clid\tType\tGeneCount\tP-value\n")
-	for f in gene_plot_files:
+	for f in gene_files:
 		gene_files[f].write("GeneID\tClid\tType\n")
 
 	for k in sorted(kc, key=int):
@@ -497,6 +495,7 @@ def results_to_file(kc, path, pids, tps, cv, ge, dt, ge_f):
 
 			# writing gene plotting data
 			for g in kc[k].pheno[p].gid:
+				ge_norm=normalize(ge[g])
 				for expr_idx in range(pidx*len(tps), (pidx*len(tps))+len(tps)):
 					tidx=expr_idx%len(tps)
 					tid=tps[tidx]
@@ -504,10 +503,12 @@ def results_to_file(kc, path, pids, tps, cv, ge, dt, ge_f):
 					gidx=kc[k].pheno[p].gid.index(g)
 
 					gene_plot_files[kc[k].type].write("%s_%s\t%s\t%s\t%s\t%f\t%s\n"%(g, p, k, p, tid, ge[g][expr_idx], kc[k].sub_type))
+					normtype="%s_norm"%(kc[k].type)
+					gene_plot_files[normtype].write("%s_%s\t%s\t%s\t%s\t%f\t%s\n"%(g, p, k, p, tid, ge_norm[expr_idx], kc[k].sub_type))
 
 
 	# closing files
-	for f in cluster_files.values()+gene_files.values()+cluster_plot_files.values()+gene_plot_files.values():
+	for f in list(cluster_files.values()) + list (gene_files.values())+ list (cluster_plot_files.values()) + list (gene_plot_files.values()):
 		f.close()
 
 
@@ -538,7 +539,4 @@ def main():
 
 if __name__=="__main__":
 	main()
-
-
-
 
